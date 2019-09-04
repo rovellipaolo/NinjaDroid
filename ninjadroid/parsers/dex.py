e0382545a339c6ddcf5455f53b5c8044fc165a2a
Ian Truslove <ian.truslove@gmail.com>
2019-09-03 21:12:42 -0600

    Use generators to make processing more efficient

--- ninjadroid/parsers/dex.py
+++ ninjadroid/parsers/dex.py
@@ -1,5 +1,5 @@
 import subprocess
-from typing import Dict, List
+from typing import Dict, List, Sequence
 
 from ninjadroid.parsers.dex_interface import DexInterface
 from ninjadroid.parsers.file import File
@@ -35,65 +35,68 @@ class Dex(File, DexInterface):
         """
         command = "strings " + self.get_file_path()
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
-        self._strings = process.communicate()[0].decode("utf-8").splitlines()
-        self._strings.sort()
-        while "" in self._strings:
-            self._strings.remove("")
-        self._strings = [s.strip() for s in self._strings]
+        strings = filter(lambda s: s != "",
+                         (s.strip() for s in process.communicate()[0].decode("utf-8").splitlines()))
+        self._strings = sorted(strings)
 
     def _extract_and_set_substring_from(self):
         """
         Extract the strings from the classes.dex file and set the correspondent attributes.
         Empty strings will be removed.
         """
-        for string in self._strings:
-            if string == "":
-                continue
+        urls = (url
+                for s in self._strings
+                for url in self._extract_urls_from(s))
+        self._urls = sorted(urls)
 
-            self._extract_and_set_urls_from(string)
-            self._extract_and_set_shell_commands_from(string)
-            #self._extract_and_set_signatures_from(string)
+        shell_commands = (command
+                          for s in self._strings
+                          for command in self._extract_shell_commands_from(s))
+        self._shell_commands = sorted(shell_commands)
+        # self._custom_signatures.sort()
 
-        self._urls.sort()
-        self._shell_commands.sort()
-        #self._custom_signatures.sort()
-
-    def _extract_and_set_urls_from(self, string: str):
+    def _extract_urls_from(self, string: str) -> Sequence[str]:
         """
-        Extract eventual URLs from a string and set the correspondent attribute.
+        Extract URLs from a string.
 
-        :param string: The string from which extracting the eventual URLs.
+        Currently at most one URL is extracted from a string. This may change.
+
+        :param string: The string from which to extract the URLs.
         """
         if not hasattr(self, "_uri"):
             self._uri_signature = URISignature()
         if len(string) > 6:
-            url = self._uri_signature.get_matches_in_string(string)
-            if url != "" and url not in self._urls:
-                self._urls.append(url)
+            match = self._uri_signature.get_matches_in_string(string)
+            if match != "":
+                return [match]
+        return []
 
-    def _extract_and_set_shell_commands_from(self, string: str):
+    def _extract_shell_commands_from(self, string: str) -> Sequence[str]:
         """
-        Extract eventual shell commands from a string and set the correspondent attribute.
+        Extract shell commands from a string.
 
-        :param string: The string from which extracting the eventual shell commands.
+        Currently at most one shell command is extracted from a string. This may change.
+
+        :param string: The string from which to extract the shell commands.
         """
         if not hasattr(self, "_shell"):
             self._shell_signature = ShellCommandSignature()
         command = self._shell_signature.get_matches_in_string(string)
-        if command != "" and command not in self._shell_commands:
-            self._shell_commands.append(command)
+        if command != "":
+            return [command]
+        return []
 
-    def _extract_and_set_signatures_from(self, string: str):
-        """
-        Extract eventual signatures from a string and set the correspondent attribute.
+    # def _extract_and_set_signatures_from(self, string: str):
+    #     """
+    #     Extract eventual signatures from a string and set the correspondent attribute.
 
-        :param string: The string from which extracting the eventual signatures.
-        """
-        if not hasattr(self, "_shell"):
-            self._generic_signature = Signature()
-            match = self._generic_signature.get_matches_in_string(string)
-            if match != "" and match not in self._custom_signatures:
-                self._custom_signatures.append(match)
+    #     :param string: The string from which extracting the eventual signatures.
+    #     """
+    #     if not hasattr(self, "_shell"):
+    #         self._generic_signature = Signature()
+    #         match = self._generic_signature.get_matches_in_string(string)
+    #         if match != "" and match not in self._custom_signatures:
+    #             self._custom_signatures.append(match)
 
     @staticmethod
     def looks_like_a_dex(filename: str) -> bool:

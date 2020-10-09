1f049352c32dfc5b707dd153fa6a2cd58ec25eee
Paolo Rovelli <rovellipaolo@gmail.com>
2020-10-09 09:57:43 +0200

    Fix ninjadroid.sh

--- ninjadroid.sh
+++ ninjadroid.sh
@@ -3,7 +3,7 @@
 case "$1" in
     ninjadroid|ninjadroid.py)
         shift
-        exec python3 $NINJADROID_DIR/ninjadroid.py $@
+        exec python3 $NINJADROID_HOME/ninjadroid.py $@
         ;;
     help)
         echo "ninjadroid ninjadroid.py|json|html|version [opts]"
@@ -17,7 +17,7 @@ case "$1" in
         apk_file=$2
         output_dir=$(mktemp -d)
         log_path=/var/log/ninjadroid/output
-        python3 $NINJADROID_DIR/ninjadroid.py -e $output_dir $apk_file >$log_path 2>&1
+        python3 $NINJADROID_HOME/ninjadroid.py -e $output_dir $apk_file >$log_path 2>&1
         output_file=$(ls $output_dir | grep "report-.*\.$format$" | head -1)
         if [[ ! -z $output_file ]] ; then
             exec cat $output_dir/$output_file
